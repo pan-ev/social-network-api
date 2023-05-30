6507ee254ef8fd385f2790beb2d1425c4dd3a5ce
epan <epan616@gmail.com>
2023-05-30 01:22:32 -0400

    Thought routes and controllers and server file

---
+++ routes/api/thoughtRoutes.js
@@ -0,0 +1,25 @@
+const router = require("express").Router();
+const {
+  getThoughts,
+  getSingleThought,
+  createThought,
+  deleteThought,
+  updateThought,
+  addReaction,
+  removeReaction,
+} = require("../../controllers/thoughtController");
+
+// /api/thoughts
+router.route("/").get(getThoughts).post(createThought);
+
+// /api/thought/:thoughtId
+router
+  .route("/:thoughtId")
+  .get(getSingleThought)
+  .put(updateThought)
+  .delete(deleteThought);
+
+// /api/thought/:thoughtId/reactions
+router.route("/:thoughtId/reactions").post(addReaction).delete(removeReaction);
+
+module.exports = router;
